6da814ad641df6f91acaa856db2b80bd0c461906
AzureGem <rreminy@gmail.com>
2024-08-25 05:48:35 -0400

    misc comment

--- AG.Collections.Concurrent/ConcurrentCounter.cs
+++ AG.Collections.Concurrent/ConcurrentCounter.cs
@@ -15,12 +15,12 @@ namespace AG.Collections.Concurrent
     // - Additional method that uses a hash value instead of deriving the address of the cellCount variable
     // - Expand cell counts to prime sizes
 
-    /// <summary>A concurrent counter keeping track of its count using concurrent <see cref="Interlocked"/> based counters</summary>
+    /// <summary>A concurrent counter keeping track of its count using concurrent <see cref="Interlocked"/> based counters.</summary>
     public sealed class ConcurrentCounter
     {
         private sealed class Cell
         {
-            [InlineArray(16)] // Cheaty way to get a whole cacheline worth of longs
+            [InlineArray(16)] // Cheaty way to get a whole cacheline worth of longs. Only [7] is used.
             internal struct CellStruct
             {
                 [SuppressMessage("Major Code Smell", "S1144", Justification = "Inline Array")]
